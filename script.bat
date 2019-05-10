50cd2aaf3d94dd9a436b39da0f1393b8d10c4b0c
Premprakash6260 <38817026+Premprakash6260@users.noreply.github.com>
2019-05-10 23:41:36 +0530

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,16 @@
+@echo off
+goto :main
+:sum
+setlocal
+	echo	sum is executing
+endlocal & set /a add=%x%+%y%
+goto :eof
+
+:main
+setlocal
+	set /p x=Enter first no
+	set /p y=Enter second no
+	call :sum
+	echo Value of x + y is %add%
+endlocal
+goto :eof
